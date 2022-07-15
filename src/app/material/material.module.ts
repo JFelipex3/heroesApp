2338855f5feea58ea5d6bf20c9b528b6b8f13e3e
Jhonnatan Machuca <jfelipe.machuca@gmail.com>
2022-07-15 16:33:38 -0400

    Formulario Crear Héroe
    Diseño de la página agregar

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -9,9 +9,9 @@ import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatSelectModule} from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
 
 
 @NgModule({
@@ -26,6 +26,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatListModule,
     MatIconModule,
     MatProgressSpinnerModule,
+    MatSelectModule,
     MatSidenavModule,
     MatToolbarModule
   ]
